ba715b5f9b22fdcced5ed0ef08f9617c7e572571
asiamarc <asiamarc@K114-1-22.dep-info.iutmontp.univ-montp2.fr>
2023-11-07 13:44:34 +0100

    ajout de la classe CommandeRemplacer

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,25 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument {
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if(parameters.length < 2) {
+            System.err.println("Format attendu : remplacer;int;int;chaine");
+            return;
+        }
+        int start = Integer.parseInt(parameters[1]);
+        int end = Integer.parseInt(parameters[2]);
+        String texte = parameters[3];
+        this.document.remplacer(start,end,texte);
+        super.executer();
+    }
+
+}
+
